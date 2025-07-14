143b6fc8c0034f1b4179bae8c68c0201f390beb4
Mehmetcan Kılınç <mehmtcankilinc@hotmail.com>
2025-07-14 20:12:32 +0300

    floating navbar

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import "./global.css";
-import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import BottomNav from "./src/navigation/BottomNav";
 import { SafeAreaProvider } from "react-native-safe-area-context";
--- src/navigation/BottomNav.js
+++ src/navigation/BottomNav.js
@@ -2,23 +2,103 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import CalenderScreen from "../screens/CalenderScreen";
 import HomeScreen from "../screens/HomeScreen";
 import PastScreen from "../screens/PastScreen";
-import EvilIcons from "@expo/vector-icons/EvilIcons";
 import AntDesign from "@expo/vector-icons/AntDesign";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
-import { widthPercentageToDP as wp } from "react-native-responsive-screen";
-import { View, Text } from "react-native";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import { View, TouchableOpacity, StyleSheet } from "react-native";
+import Animated, {
+  FadeIn,
+  FadeOut,
+  LinearTransition,
+} from "react-native-reanimated";
+
+const AnimatedTouchableOpacity =
+  Animated.createAnimatedComponent(TouchableOpacity);
+
+const PRIMARY_COLOR = "#130057";
+const SECONDARY_COLOR = "#fff";
 
 const Tab = createBottomTabNavigator();
 
-function BottomNav() {
-  const insets = useSafeAreaInsets();
+function CustomBottomNav({ state, descriptors, navigation }) {
+  return (
+    <View style={styles.container}>
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+              ? options.title
+              : route.name;
 
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: "tabPress",
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name, route.params);
+          }
+        };
+
+        return (
+          <AnimatedTouchableOpacity
+            layout={LinearTransition.springify().mass(0.5)}
+            key={route.key}
+            onPress={onPress}
+            style={[
+              styles.tabItem,
+              { backgroundColor: isFocused ? SECONDARY_COLOR : "transparent" },
+            ]}
+          >
+            {getIconByRouteName(
+              route.name,
+              isFocused ? PRIMARY_COLOR : SECONDARY_COLOR
+            )}
+            {isFocused && (
+              <Animated.Text
+                entering={FadeIn.duration(200)}
+                exiting={FadeOut.duration(200)}
+                style={styles.text}
+              >
+                {label}
+              </Animated.Text>
+            )}
+          </AnimatedTouchableOpacity>
+        );
+      })}
+    </View>
+  );
+
+  function getIconByRouteName(routeName, color) {
+    switch (routeName) {
+      case "HomeScreen":
+        return <AntDesign name="home" size={24} color={color} />;
+      case "CalendarScreen":
+        return <AntDesign name="calendar" size={24} color={color} />;
+      case "PastScreen":
+        return <AntDesign name="folderopen" size={24} color={color} />;
+      default:
+        return <AntDesign name="home" size={24} color={color} />;
+    }
+  }
+}
+
+function BottomNav() {
   return (
     <Tab.Navigator
+      tabBar={(props) => <CustomBottomNav {...props} />}
       screenOptions={{
         tabBarShowLabel: false,
-        headerTitle: "stalker",
+        headerTitle: "Stalker",
         headerStyle: {
           backgroundColor: "#f9f6ed",
           elevation: 0,
@@ -26,7 +106,7 @@ function BottomNav() {
           borderBottomWidth: 0,
         },
         headerTitleAlign: "center",
-        headerTintColor: "#3750a0",
+        headerTintColor: "#130057",
         headerTitleStyle: {
           fontWeight: "bold",
         },
@@ -40,121 +120,58 @@ function BottomNav() {
             <MaterialCommunityIcons name="crown" size={30} color="black" />
           </View>
         ),
-        tabBarStyle: {
-          position: "absolute",
-          bottom: insets.bottom + wp("2%"),
-          marginLeft: wp("5%"),
-          backgroundColor: "#f9f6ed",
-          borderRadius: 20,
-          height: wp("14%"),
-          elevation: 4,
-          shadowColor: "#000",
-          width: wp("90%"),
-          shadowOpacity: 0.1,
-          shadowOffset: {
-            width: 0,
-            height: 3,
-          },
-          shadowRadius: 4,
-        },
       }}
     >
       <Tab.Screen
         name="PastScreen"
         component={PastScreen}
-        options={{
-          tabBarInactiveTintColor: "gray",
-          tabBarActiveTintColor: "#3750a0",
-          tabBarIcon: ({ focused, size, color }) => (
-            <View
-              style={{
-                alignItems: "center",
-                justifyContent: "center",
-                top: wp("1%"),
-              }}
-            >
-              {focused ? (
-                <View
-                  style={{
-                    gap: wp("2%"),
-                    width: wp("22%"),
-                    height: wp("7%"),
-                    marginTop: wp("2%"),
-                  }}
-                  className="justify-center items-center flex-row bg-slate-400"
-                >
-                  <EvilIcons
-                    name="archive"
-                    size={34}
-                    color={focused ? "#3750a0" : "gray"}
-                  />
-                  <Text
-                    style={{
-                      marginTop: wp("2%"),
-                    }}
-                  >
-                    Past
-                  </Text>
-                </View>
-              ) : (
-                <EvilIcons
-                  name="archive"
-                  size={34}
-                  color={focused ? "#3750a0" : "gray"}
-                />
-              )}
-            </View>
-          ),
-        }}
+        options={{ title: "Past" }}
       />
       <Tab.Screen
         name="HomeScreen"
         component={HomeScreen}
-        options={{
-          tabBarInactiveTintColor: "gray",
-          tabBarActiveTintColor: "#3750a0",
-          tabBarIcon: ({ focused, size, color }) => (
-            <View
-              style={{
-                alignItems: "center",
-                justifyContent: "center",
-                top: wp("2%"),
-              }}
-            >
-              <AntDesign
-                name="home"
-                size={24}
-                color={focused ? "#3750a0" : "gray"}
-              />
-            </View>
-          ),
-        }}
+        options={{ title: "Home" }}
       />
       <Tab.Screen
-        name="CalenderScreen"
+        name="CalendarScreen"
         component={CalenderScreen}
-        options={{
-          tabBarInactiveTintColor: "gray",
-          tabBarActiveTintColor: "#3750a0",
-          tabBarIcon: ({ focused, size, color }) => (
-            <View
-              style={{
-                alignItems: "center",
-                justifyContent: "center",
-                top: wp("2%"),
-              }}
-            >
-              <AntDesign
-                name="calendar"
-                size={24}
-                color={focused ? "#3750a0" : "gray"}
-              />
-            </View>
-          ),
-        }}
+        options={{ title: "Calendar" }}
       />
     </Tab.Navigator>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    position: "absolute",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: PRIMARY_COLOR,
+    width: wp("80%"),
+    alignSelf: "center",
+    bottom: hp("6%"),
+    borderRadius: 40,
+    paddingVertical: wp("4%"),
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 5 },
+    shadowOpacity: 0.3,
+    shadowRadius: 5,
+    gap: wp("3%"),
+  },
+  tabItem: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    height: hp("4.5%"),
+    paddingHorizontal: wp("3%"),
+    borderRadius: 30,
+  },
+  text: {
+    color: PRIMARY_COLOR,
+    marginLeft: wp("2%"),
+    fontWeight: "500",
+  },
+});
 
 export default BottomNav;
